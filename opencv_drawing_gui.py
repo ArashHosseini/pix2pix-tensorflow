50982f682fd17c47ac574d7ecf829b439b33f06d
ArashHosseini <seyed_ahmad.hosseini@filmakademie.de>
2017-12-03 10:46:47 +0100

    merge commit, will delete,remove soonish, only for visual merge result

--- opencv_drawing_gui.py
+++ opencv_drawing_gui.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+'''
+Created on Dec 1, 2017
 
+@author: flyn
+'''
+import os
 import cv2
 import numpy as np
+from utility import inference_utility, inference_config
 
-rect_temp = []
+rect_endpoint_tmp = []
 rect_bbox = []
 drawing = False
 col = (0,0,255)
-
 img = np.zeros((553,512,3), np.uint8)
 img[:] = (13,61,251)[::-1]
-
 drawing_area = [(-1000,40), (1000, 1000)]
 cv2.rectangle(img, (0, 0), (512, 40), (255,255,255), -1)
-
 box_size = (47,30)
 
 # color buttons
@@ -31,7 +32,6 @@ buttons_dict = {"Wall": (13,61,251),
                 "Entrance": (0,201,255)}
 
 color_positions = {}
-
 for k,v in enumerate(buttons_dict.items()):
 
     button_color = v[1][::-1]
@@ -44,13 +44,26 @@ for k,v in enumerate(buttons_dict.items()):
     cv2.rectangle(img, first_point, second_point, button_color, -1)
     cv2.putText(img, button_text, (((k+1)*box_size[0]) - box_size[0], box_size[1]+8), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,0))
 
-def draw_rectangle(event, x, y, flags, param):
+
+def send_to_process():
+    resized_image = cv2.resize(img[41:553, 0:512], (256, 256))
+    cv2.imwrite(tmp_, resized_image)
+    while post.empty():
+        post.put(tmp_)
+        return
     
-        global rect_bbox, rect_temp, drawing, col
+def get_from_process():
+    while not get.empty():
+        inference = get.get()
+        inference_read = cv2.imread(inference)
+        cv2.imshow('INFERENCE', inference_read)
+
+
+def draw_rectangle(event, x, y, flags, param):
+        global rect_bbox, rect_endpoint_tmp, drawing, col
     
         if event == cv2.EVENT_LBUTTONDOWN:
-            rect_temp = []
-            
+            rect_endpoint_tmp = []
             for color, position in color_positions.items():
 
                 if x in range(position[1][0], position[0][0]) and y in range(position[0][1], position[1][1]):
@@ -64,9 +77,10 @@ def draw_rectangle(event, x, y, flags, param):
                 drawing = True
 
         elif event == cv2.EVENT_LBUTTONUP:
-
             if drawing_area[0][0] < x < drawing_area[1][0] and drawing_area[0][1] < y < drawing_area[1][1]:
-
+                
+                send_to_process()
+                
                 rect_bbox.append((x, y))
                 drawing = False
             
@@ -86,39 +100,43 @@ def draw_rectangle(event, x, y, flags, param):
                 if (lx, ty) != (rx, by):
                     bbox = [lx, ty, rx, by]
                    
-                # print ("Drawing Done") #callback when drawing is done
+                # print ("Drawing Done") ################################# ARASH - callback when drawing is done
 
         elif event == cv2.EVENT_MOUSEMOVE and drawing:
             if drawing_area[0][0] < x < drawing_area[1][0] and drawing_area[0][1] < y < drawing_area[1][1]:
-                rect_temp = [(x, y)]
+                rect_endpoint_tmp = [(x, y)]
 
+    
 img_copy = img.copy()
 cv2.namedWindow('pix2pix')
+tmp_ = inference_config.tmp_file
 cv2.setMouseCallback('pix2pix', draw_rectangle)
 
+post,get,pool,killer = inference_utility.process_handler()
+
 while True:
-
-    if not drawing:
-        cv2.imshow('pix2pix', img)
-
-    elif drawing and rect_temp:
-        rect_cpy = img.copy()
-        start_point = rect_bbox[0]
-        end_point_tmp = rect_temp[0]
-        cv2.rectangle(rect_cpy, start_point, end_point_tmp, color=col, thickness=-1)
-        cv2.imshow('pix2pix', rect_cpy)
-
-        # print ("Drawing") #callback during drawing
-
     key = cv2.waitKey(1) & 0xFF
-
+    
+    get_from_process()
+    
+    if drawing and rect_endpoint_tmp:
+        img = img.copy()
+        start_point = rect_bbox[0]
+        end_point_tmp = rect_endpoint_tmp[0]
+        cv2.rectangle(img, start_point, end_point_tmp, color=col, thickness=-1)
+        cv2.imshow('pix2pix', img)
+    
     if key == ord('q'):
+        killer.value = True
+        for _process in pool:
+            print "killing process", _process
+            _process.join()
         break
 
     if key == ord('s'):
         resized_image = cv2.resize(img[41:553, 0:512], (256, 256))
-        cv2.imwrite("image.jpg", resized_image)
+        cv2.imwrite(tmp_, resized_image)
         print ("Image saved!, go for tensorflow")
 
-
+    cv2.imshow('pix2pix', img)
 cv2.destroyAllWindows()
